c1f09a81ebd51545e3e990eedafa0aa9ddbaa457
CapinAhab <hazzar.maynard@protonmail.com>
2024-06-07 16:51:33 +0100

    Updated font and font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Luxi Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Luxi Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
